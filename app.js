1612a5c43be8de1a4114f388a49f47346bba1c9a
Anas Jawed <anasjawedbmj@gmail.com>
2024-10-15 20:46:44 +0300

    asignmentjs chapter12-13

---
+++ app.js
@@ -0,0 +1,95 @@
+// 2 . Write a JavaScript program that accept two integers and
+// display the larger. Also show if the two integers are equal.
+
+
+// let firstNumber = +prompt("Enter the first Integer");
+// let secondNumber = +prompt("Enter the second Integer");
+
+// if(firstNumber > secondNumber){
+//     console.log(`${firstNumber} is larger than ${secondNumber}`);
+// } else if(firstNumber < secondNumber){
+//     console.log(`${secondNumber} is larger than ${firstNumber}`)
+
+// } else{
+//     console.log(`both number are equal ${firstNumber} and ${secondNumber}`);
+// }
+
+// 3. Write a program that takes input a number from user state whether the number is positive, negative or zero.
+
+// let userguess = +prompt("Enter the number");
+
+// if(userguess > 0){
+//     console.log(`user number ${userguess} is positive`);
+// } else if(userguess < 0){
+//     console.log(`user number ${userguess} is negative`);
+
+// } else{
+//     console.log(`user guess is ${userguess}`);
+// }
+
+// 4. Write a program that takes a character (i.e. string of 
+//     length 1) and returns true if it is a vowel, false otherwise
+
+
+// let userInput = prompt("Enter a single character");
+
+// if(userInput.length !== 1){
+//     console.log("Please enter a single charcter");
+// } else{
+//     let char = userInput.toLowerCase();
+
+//     if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+//         console.log(`${char} is a vowel`); 
+         
+//     } else{
+//         console.log(`${char} is not a vowel`);
+//     }
+// }
+
+
+// 5. Write a program that
+// a. Store correct password in a JS variable.
+// b. Asks user to enter his/her password
+// c. Validate the two passwords:
+// i. Check if user has entered password. If not, then 
+// give message “ Please enter your password”
+// ii. Check if both passwords are same. If they are 
+// same, show message “Correct! The password you 
+// entered matches the original password”. Show 
+// “Incorrect password” otherwise.
+
+
+// let password = "saylani123";
+// let user = prompt("Enter Your Password");
+
+// if(!user){
+//     console.log(prompt("Enter your password"));
+// } else if(user === password){
+//     console.log("Correct! The password you entered mathches the original password")
+// } else{
+//     console.log("Incorrect Password");
+// }
+
+
+// 6. This if/else statement does not work. Try to fix it:
+
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18){
+//     console.log("good day");
+// } else{
+//     console.log("good evening");
+// }
+
+// 7. Write a program that takes time as input from user in 24 
+// hours clock format like: 1900 = 7pm. Implement the 
+// following case using if, else & else if statements
+
+// let userTime = +prompt("Enter your time");
+
+// if(userTime === 1900){
+//     console.log("Its time now 7pm");
+// } else{
+//     console.log("remaining time");
+// }
